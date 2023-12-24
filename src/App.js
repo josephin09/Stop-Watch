46bbecbef5e5b5d0b16d6292873db2da5fb0bd65
josephin09 <josephindavidlatha@gmail.com>
2023-12-24 11:56:14 +0530

    changesdone

--- src/App.js
+++ src/App.js
@@ -1,63 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; 
+import React, { useState, useEffect } from "react";
 
-const Stopwatch = () => {
-  const [isRunning, setIsRunning] = useState(false);
-  const [time, setTime] = useState(0);
+const App = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
-    let interval;
-
-    if (isRunning) {
+    let interval = null;
+    if (isActive) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
+        setSeconds((prevSeconds) => prevSeconds + 1);
       }, 1000);
-    } else {
+    } else if (!isActive && seconds !== 0) {
       clearInterval(interval);
     }
-
     return () => clearInterval(interval);
-  }, [isRunning]);
-
-  const startStopwatch = () => {
-    setIsRunning(!isRunning);
-  };
-
-  const resetStopwatch = () => {
-    setIsRunning(false);
-    setTime(0);
-  };
+  }, [isActive, seconds]);
 
   const formatTime = (timeInSeconds) => {
     const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = timeInSeconds % 60;
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const remainingSeconds = timeInSeconds % 60;
+
+    const formattedMinutes = String(minutes).padStart(1, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
     return `${formattedMinutes}:${formattedSeconds}`;
+
+  };
+
+  const handleStartStop = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setSeconds(0);
   };
 
   return (
-    <div className="stopwatch-container">
-      <h1>Stopwatch</h1>
-      <div className="time-display">
-        <p>Time: {formatTime(time)}</p>
-      </div>
-      <div className="button-container">
-        <button className="start-stop-button" onClick={startStopwatch}>
-          {isRunning ? 'Stop' : 'Start'}
-        </button>
-        <button className="reset-button" onClick={resetStopwatch}>
-          Reset
-        </button>
-      </div>
-    </div>
-  );
-};
-
-const App = () => {
-  return (
-    <div className="app-container">
-      <Stopwatch />
+    <div>
+        <h1>Stopwatch</h1>
+      <p>Time: &nbsp; {formatTime(seconds)}</p>
+      <button onClick={handleStartStop}>{isActive ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
